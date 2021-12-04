5e51704be15a9ad63bce51c6a0fcbd6a00d7fead
Kuba Nowak <kubanowak9755@gmail.com>
2021-12-04 15:29:21 +0100

    Handle skills radio buttons

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,26 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
+
+const skillLabels = document.querySelectorAll(".skills__tab-label--js");
+const skillLists = document.querySelectorAll(".skills__list--js");
+
+const toggleListPicker = (picker) => {
+  skillLabels.forEach((label) => {
+    label.classList.remove("skills__tab-label--selected");
+  });
+  picker.classList.add("skills__tab-label--selected");
+};
+
+const toggleListVisibility = (idPrefix) => {
+  skillLists.forEach((list) => {
+    list.classList.add("skills__list--hidden");
+  });
+  let visibleList = document.querySelector(`#${idPrefix}-list--js`);
+  visibleList.classList.remove("skills__list--hidden");
+};
+
+skillLabels.forEach((el) => {
+  el.addEventListener("click", (e) => {
+    toggleListPicker(el);
+    toggleListVisibility(el.htmlFor);
+  });
+});
