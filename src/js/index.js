589791d810b2f0c90b14149e50cf502ae4595b36
Kuba Nowak <kubanowak9755@gmail.com>
2021-12-04 15:40:53 +0100

    Handle skill list select correctly

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ import "../scss/main.scss";
 
 const skillLabels = document.querySelectorAll(".skills__tab-label--js");
 const skillLists = document.querySelectorAll(".skills__list--js");
+const skillListSelect = document.querySelector(".skills__select--js");
 
 const toggleListPicker = (picker) => {
   skillLabels.forEach((label) => {
@@ -18,9 +19,22 @@ const toggleListVisibility = (idPrefix) => {
   visibleList.classList.remove("skills__list--hidden");
 };
 
+const changeListSelectValue = (newValue) => {
+  skillListSelect.value = newValue;
+};
+
 skillLabels.forEach((el) => {
   el.addEventListener("click", (e) => {
     toggleListPicker(el);
     toggleListVisibility(el.htmlFor);
+    changeListSelectValue(el.htmlFor);
   });
+});
+
+skillListSelect.addEventListener("change", (e) => {
+  let skillsListPicker = document.querySelector(
+    `label[for="${e.target.value}"]`
+  );
+  toggleListPicker(skillsListPicker);
+  toggleListVisibility(e.target.value);
 });
